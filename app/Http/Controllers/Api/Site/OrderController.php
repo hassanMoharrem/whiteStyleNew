3b90d1b97462a68a9ec9a6fa47545a94afa5f37f
hassanMoharrem <mhrm781@gmail.com>
2026-06-17 16:36:01 +0300

    hassan

--- app/Http/Controllers/Api/Site/OrderController.php
+++ app/Http/Controllers/Api/Site/OrderController.php
@@ -70,7 +70,6 @@ class OrderController extends Controller
         $commission = $request->delivery_cost - $deliveryPrice;
         $sub_total_edit= $request->subtotal + $commission;
         $total = $sub_total_edit + $deliveryPrice;
-        dd($total);
         $order = Order::create([
             'customer_name' => $request->customer_name,
             'customer_phone' => $request->customer_phone,
